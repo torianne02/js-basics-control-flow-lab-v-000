1f488864dd5e8b02580eff8da4921eeb2ea59df2
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-14 00:20:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    result = "This one is on me!"
+  }
+  else if (feet > 2000 && feet < 2500) {
+    result = "I will gladly take your thirty bucks."
+  }
+  else {
+    result = "No can do."
+  }
+  return result
+}
+
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+}
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
