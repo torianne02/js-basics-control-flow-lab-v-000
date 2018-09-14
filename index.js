4d581b25038f8ef53b726c684405372170b33f20
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-14 00:13:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function scuberGreetingForFeet(feet) {
   if (feet <= 400) {
     result = "This one is on me!"
   }
-  else if (feet > 2000) {
+  else if (feet > 2000 && feet < 2500) {
     result = "I will gladly take your thirty bucks."
   }
-  else if (feet > 2500) {
+  else {
     result = "No can do."
   }
   return result
