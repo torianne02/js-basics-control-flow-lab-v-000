47cb6fb99f07bd6be34afb9f222a578d9c5abe63
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-14 00:10:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ function scuberGreetingForFeet(someValue) {
   if (someValue <= 400) {
     result = "This one is on me!"
   }
+  else if (someValue > 2000) {
+    result = "I will gladly take your thirty bucks."
+  }
   return result
-}
+};
