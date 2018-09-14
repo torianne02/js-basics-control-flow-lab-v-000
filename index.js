538b1f9bc7877121f73e50bff51c552cf54ef917
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-14 00:20:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,14 @@ function scuberGreetingForFeet(feet) {
 function ternaryCheckCity(city) {
   return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default: 
+      return 'Bye.';
+  }
+}
